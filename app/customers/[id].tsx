96de81c78283b9875856e2acd4162bd416e0b69f
Patrik <macpatrik1@gmail.com>
2024-03-04 16:37:10 +0100

    second init

--- app/customers/[id].tsx
+++ app/customers/[id].tsx
@@ -1,12 +1,69 @@
-import { useLocalSearchParams } from 'expo-router';
-import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { CameraView, Camera } from "expo-camera/next";
 
-const Customer = () => {
-  const local = useLocalSearchParams();
+export default function QRScanner({handleBarCodeScanned} : {handleBarCodeScanned : Function}) {
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [scanned, setScanned] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  // @ts-ignore
+  const handleScanned = ({ type, data}) => {
+    handleBarCodeScanned(type, data);
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (!hasPermission) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <Text>{local.id}</Text>
-  )
+      <View style={styles.container}>
+        {/* Container for the camera view with border and rounded corners */}
+        <View style={styles.cameraContainer}>
+          <CameraView
+              onBarcodeScanned={scanned ? undefined : handleScanned}
+              barcodeScannerSettings={{
+                barcodeTypes: ["qr", "pdf417"],
+              }}
+              style={{ flex: 1 }} // Use flex to fill the container
+          />
+        </View>
+
+        {scanned && (
+            <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+        )}
+      </View>
+  );
 }
 
-export default Customer
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative"
+  },
+  cameraContainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "white",
+    overflow: "hidden",
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
-import { Link, useRouter } from 'expo-router';
+import {Link, useRouter} from "expo-router";
+import React from "react";
+import {Button, TouchableOpacity} from "react-native";
 
-const Home: React.FC = () => {
-  console.log("home");
-  const router = useRouter()
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10, backgroundColor: '#f1f1f1' }}>
-        <Text>BOK</Text>
-        <TouchableOpacity
-              onPress={() => {
-                router.push(`/customers/2`)
-              }}>
-              <Text>2</Text>
-            </TouchableOpacity>
-      </View>
-      {/* Rest of your Home content here */}
-    </SafeAreaView>
-  );
-};
+const Home = () => {
 
-export default Home;
+    return (
+        <Link href={"/login"} asChild>
+            <Button title={"login"}/>
+        </Link>
+    )
+}
+
+export default Home
